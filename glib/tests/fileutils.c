ce60efe97933bafdeaf1cf059829d9842e5d9319
Dan Nicholson <nicholson@endlessm.com>
2014-05-09 16:17:22 -0700

    tests: Handle root changing to non-executable directories
    test_stdio_wrappers () asserts that changing to a directory without
    executable permissions (0666 in this case) will fail. However, if the
    test is run as root, it will pass since root bypasses permissions. See
    path_resolution(7).

--- glib/tests/fileutils.c
+++ glib/tests/fileutils.c
@@ -864,10 +864,19 @@ test_stdio_wrappers (void)
 
   cwd = g_get_current_dir ();
   path = g_build_filename (cwd, "mkdir-test", NULL);
-  g_free (cwd);
   ret = g_chdir (path);
-  g_assert_cmpint (errno, ==, EACCES);
-  g_assert_cmpint (ret, ==, -1);
+  if (geteuid () == 0)
+    {
+      /* root can change to non-executable directories */
+      g_assert (ret == 0);
+      g_chdir (cwd);
+    }
+  else
+    {
+      g_assert (errno == EACCES);
+      g_assert (ret == -1);
+    }
+  g_free (cwd);
   ret = g_chmod (path, 0777);
   g_assert_cmpint (ret, ==, 0);
   ret = g_chdir (path);
