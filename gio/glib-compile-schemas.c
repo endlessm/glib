258f15d4d8bfeb931b8b702ea979aa7d5e1f0c3c
Iain Lane <iain.lane@canonical.com>
2017-11-23 18:51:57 +0000

    Disable confusing (to users) warning about deprecated schema paths
    Forwarded: not-needed

--- gio/glib-compile-schemas.c
+++ gio/glib-compile-schemas.c
@@ -1233,6 +1233,9 @@ parse_state_start_schema (ParseState  *state,
       return;
     }
 
+  // Disable this warning: it confuses users and there is unlikely to be much
+  // progress towards fixing
+  /*
   if (path && (g_str_has_prefix (path, "/apps/") ||
                g_str_has_prefix (path, "/desktop/") ||
                g_str_has_prefix (path, "/system/")))
@@ -1245,6 +1248,7 @@ parse_state_start_schema (ParseState  *state,
       g_printerr ("%s\n", message);
       g_free (message);
     }
+    */
 
   state->schema_state = schema_state_new (path, gettext_domain,
                                           extends, extends_name, list_of);
